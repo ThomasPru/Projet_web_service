37d22c0cbe6b69224956275e38a55b01d4d5c2be
tprunier <thomas.23@live.fr>
2018-05-23 14:09:08 +0200

    first commit - rendu du projet

---
+++ src/app.js
@@ -0,0 +1,81 @@
+
+
+    var app = require('express')(),
+        server = require('http').createServer(app),
+        io = require('socket.io').listen(server),
+        ent = require('ent'), // Permet de bloquer les caractères HTML (sécurité équivalente à htmlentities en PHP)
+        fs = require('fs');
+       
+        var session = require('cookie-session'); // Charge le middleware de sessions
+        var bodyParser = require('body-parser'); // Charge le middleware de gestion des paramètres
+        var urlencodedParser = bodyParser.urlencoded({ extended: false });
+       
+       
+     
+    // Chargement de la page index.html
+    app.get('/', function (req, res) {
+      res.sendfile(__dirname + '/todo.html');
+    });
+     
+     
+     
+    io.sockets.on('connection', function (socket, pseudo) {
+        // Dès qu'on nous donne un pseudo, on le stocke en variable de session et on informe les autres personnes
+        socket.on('nouveau_client', function(pseudo) {
+            pseudo = ent.encode(pseudo);
+            socket.pseudo = pseudo;
+            socket.broadcast.emit('nouveau_client', pseudo);
+        });
+     
+        // Dès qu'on reçoit un message, on récupère le pseudo de son auteur et on le transmet aux autres personnes
+        socket.on('message', function (message) {
+            message = ent.encode(message);
+            socket.broadcast.emit('message', {pseudo: socket.pseudo, message: message});
+        });
+    });
+     
+     
+     
+    /* On utilise les sessions */
+    app.use(session({secret: 'todotopsecret'}))
+     
+     
+     
+    /* S'il n'y a pas de todolist dans la session,
+    on en crée une vide sous forme d'array avant la suite */
+    .use(function(req, res, next){
+        if (typeof(req.session.todolist) == 'undefined') {
+            req.session.todolist =  [];
+        }
+        next();
+    })
+     
+    /* On affiche la todolist et le formulaire */
+    .get('/todo', function(req, res) {
+        res.render('todo.ejs', {todolist: req.session.todolist});
+    })
+     
+    /* On ajoute un élément à la todolist */
+    .post('/todo/ajouter/', urlencodedParser, function(req, res) {
+        if (req.body.newtodo != '') {
+            req.session.todolist.push(req.body.newtodo);
+        }
+        res.redirect('/todo');
+    })
+     
+    /* Supprime un élément de la todolist */
+    .get('/todo/supprimer/:id', function(req, res) {
+        if (req.params.id != '') {
+            req.session.todolist.splice(req.params.id, 1);
+        }
+        res.redirect('/todo');
+    })
+     
+    /* On redirige vers la todolist si la page demandée n'est pas trouvée */
+    .use(function(req, res, next){
+        res.redirect('/todo');
+    })
+     
+     
+    server.listen(8080);
+
